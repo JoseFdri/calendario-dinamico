e59f0aafd0bc1723d16ed64136a889c518f512ec
JoseFdri <driferjose@gmail.com>
2018-03-17 20:38:17 -0500

    [add]-metodos para contar semanas y obtener orden de los dias de un mes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
- 
+  public startDate
+  public numberDays
+  public countryCode
+  public meses = []
+
+  private start : Date
+  private end : Date
+  private runningDate: Date
+  private pintar = false
+
+  private contar_semanas_de_mes(date: Date) {
+    var anio = date.getFullYear()
+    var mes = date.getMonth()
+    var totalDiasDelMes = new Date(anio, mes +1, 0).getDate();
+
+    var numSemanas = 0 ;
+    for (var i = 1 ; i <= totalDiasDelMes; i++) {
+        var ordenDia = this.obtener_orden_dia(anio,mes,i).getDay();
+        if(ordenDia == 6){
+            numSemanas++;
+        }
+        if(i == totalDiasDelMes) {
+          if(ordenDia != 6) {
+            numSemanas++
+          }
+        }
+    }
+    return numSemanas;
+  }
+
+  private obtener_orden_dia(anio, mes, dia) {
+    var date = new Date(anio, mes);
+    return new Date(date.setDate(dia));
+  }
 }
