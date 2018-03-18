87bbc4baad233be694db29c60027b09b7a504bfe
JoseFdri <driferjose@gmail.com>
2018-03-17 20:50:42 -0500

    [ADD]-maquetando interfaz e implementando metodos para la creacion de cada mes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,30 @@ export class AppComponent {
   private end : Date
   private runningDate: Date
   private pintar = false
+  
+  private crear_mes(fecha : Date ) {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ]
+    let modeloMes = {
+      nombre: monthNames[fecha.getMonth()] + ' '+ fecha.getFullYear(),
+      semanas : []
+    }
+    const numDias = 7
+    const semanas = this.contar_semanas_de_mes(fecha)
+    for (let i = 0; i < semanas; i++) {
+      var semana = []
+      for (let a = 0; a < numDias; a++) {
+        let modeloDia = {
+          fecha: '',
+          color: a == 0 || a == 6 ? 'yellow' : 'grey'
+        }
+        semana.push(modeloDia)
+      }
+      modeloMes.semanas.push(semana)
+    }
+    return modeloMes
+  }
 
   private contar_semanas_de_mes(date: Date) {
     var anio = date.getFullYear()
